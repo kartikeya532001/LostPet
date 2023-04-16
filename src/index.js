70d0abc0e5b95a8865a10611dc5892ca7275eb6d
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2023-04-16 18:36:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './Assets/CSS/index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import io from 'socket.io-client';
-export const socket = io.connect("http://localhost:5001");
+export const socket = io.connect(process.env.REACT_APP_url_socket);
 
 
 console.log(process.env.REACT_APP_url_backend);
