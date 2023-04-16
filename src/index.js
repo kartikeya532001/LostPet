453a51b140b1a97497409fdb2ada463c32019827
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2023-04-16 18:03:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './Assets/CSS/index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import io from 'socket.io-client';
-//require('dotenv').config();
 export const socket = io.connect("http://localhost:5001");
 
 
-console.log(process.env.url_backend);
+console.log(process.env.REACT_APP_url_backend);
+console.log(process.env.REACT_APP_url_socket);
 
 ReactDOM.render(
     <>
